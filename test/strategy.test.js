c582da5c7f073f4672060d7c3e054b5425e21588
amoskyler <amos.kyler@gmail.com>
2014-07-17 14:49:39 -0700

    changes to test. Still broken

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,65 +1,65 @@
 var chai = require('chai');
 global.expect = chai.expect;
-var CloudupStrategy = require('../lib/strategy');
+var rdioStrategy = require('../lib/strategy');
 
 describe('Strategy', function() {
-    
-  var strategy = new CloudupStrategy({
+
+  var strategy = new rdioStrategy({
       clientID: 'ABC123',
       clientSecret: 'secret'
     },
     function() {});
-    
-  it('should be named cloudup', function() {
-    expect(strategy.name).to.equal('cloudup');
+
+  it('should be named rdio', function() {
+    expect(strategy.name).to.equal('rdio');
   });
-  
+
   it('should have default user agent', function() {
-    expect(strategy._oauth2._customHeaders['User-Agent']).to.equal('passport-cloudup');
+    expect(strategy._oauth2._customHeaders['User-Agent']).to.equal('passport-rdio');
   });
-  
-  
+
+
   describe('constructed with user agent option', function() {
-    
-    var strategy = new CloudupStrategy({
+
+    var strategy = new rdioStrategy({
         clientID: 'ABC123',
         clientSecret: 'secret',
         userAgent: 'example.com'
       },
       function() {});
-  
+
     it('should have default user agent', function() {
       expect(strategy._oauth2._customHeaders['User-Agent']).to.equal('example.com');
     });
   });
-  
+
   describe('constructed with custom headers including user agent', function() {
-    
-    var strategy = new CloudupStrategy({
+
+    var strategy = new rdioStrategy({
         clientID: 'ABC123',
         clientSecret: 'secret',
         customHeaders: { 'User-Agent': 'example.net' }
       },
       function() {});
-  
+
     it('should have default user agent', function() {
       expect(strategy._oauth2._customHeaders['User-Agent']).to.equal('example.net');
     });
   });
-  
+
   describe('constructed with both custom headers including user agent and user agent option', function() {
-    
-    var strategy = new CloudupStrategy({
+
+    var strategy = new rdioStrategy({
         clientID: 'ABC123',
         clientSecret: 'secret',
         customHeaders: { 'User-Agent': 'example.org' },
         userAgent: 'example.net'
       },
       function() {});
-  
+
     it('should have default user agent', function() {
       expect(strategy._oauth2._customHeaders['User-Agent']).to.equal('example.org');
     });
   });
-  
+
 });
